a63dd39b1da27f41a459b84b19ceacfe1c3ae66b
bigrockquiz <robert.sundin1996@gmail.com>
2022-02-15 21:06:17 +0100

    Text design test // try button

--- app/src/main/java/com/example/firstjetpackapp/MainActivity.kt
+++ app/src/main/java/com/example/firstjetpackapp/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.firstjetpackapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstjetpackapp.ui.theme.FirstJetpackAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +23,7 @@ class MainActivity : ComponentActivity() {
             FirstJetpackAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Android", "Robert")
                 }
             }
         }
@@ -25,14 +31,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, robert: String) {
+    Text(text = "Hello $robert!", fontSize = 30.sp, color = Color.Blue, textAlign = TextAlign.Right)
+}
+
+@Composable
+fun myButton (name: String) {
+    Button(
+        onClick = { println("Hello") },
+
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 20.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+
+    ) {
+        Text("Hey") // fix error
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FirstJetpackAppTheme {
-        Greeting("Android")
+        Greeting("Android", "Robert")
     }
+}
+
+@Composable
+fun printText() {
+    println("Hello")
 }
